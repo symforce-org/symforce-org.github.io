344d10f2b4f1a703fd6971b2759eea5de76f88c9
symforce-org <bradley.solliday@skydio.com>
2025-07-14 16:50:02 +0000

    Regenerate docs from 554af60c549e1d563a7fec51308dc473f6170583

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.1.dev23+g7632a16',
+    VERSION: '0.10.1.dev28+g554af60',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
